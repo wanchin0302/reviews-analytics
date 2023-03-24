3685e9670b8366137739e74d4af09d5267bd80dc
wanchin0302 <128444456+wanchin0302@users.noreply.github.com>
2023-03-24 10:18:30 +0800

    Add files via upload

---
+++ read_ave.py
@@ -0,0 +1,15 @@
+data = []
+count = 0
+with open("reviews.txt", "r") as f:
+	 for line in f:
+	 	data.append(line)
+	 	count =+ 1
+	 	if count % 1000 == 0:  # %餘數，1000的倍數 
+	 	    print(len(data))  #每1000筆，印一次計數
+print("檔案已讀取完，共有"，len(data)，"筆資料")
+
+#算留言平均長度
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print("留言的平均長度為: "，sum_len)
